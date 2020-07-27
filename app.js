b918410f711f92fd805263f5416417b113bd26db
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-07-27 08:25:26 +0300

    add static

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  // TODO delete console.log
+  console.log(`App listening on port ${PORT}`);
+  console.log(__dirname + '/public');
 });
