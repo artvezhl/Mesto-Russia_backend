594ee12764a9844fbed5b254f7770a957bda1b9b
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-08-04 15:10:13 +0300

    connect app.js to mongo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 const users = require('./routes/users.js');
 const cards = require('./routes/cards.js');
 const unfoundPage = require('./middlewares/unfound.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.use(express.static(__dirname, '/public'));
 app.use('/users', users);
