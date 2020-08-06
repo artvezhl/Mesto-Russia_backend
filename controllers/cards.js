b32f8af36611ccb2a47da089cb4ea04ccfde38c4
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-08-06 22:45:22 +0300

    add cardLike and cardDislike

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,5 +22,27 @@ module.exports.createCard = (req, res) => {
 module.exports.removeCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send(err.message));
+    .catch(err => res.status(500).send(err.message))
+}
+
+// постановка лайка карточки
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {$addToSet: {likes: req.user._id}}, // добавить _id в массив, если его там нет
+    {new: true},
+  )
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send(err.message))
+}
+
+// снятие лайка карточки
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    {$pull: {likes: req.user._id}}, // убрать _id из массива
+    {new: true},
+  )
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send(err.message))
 }
